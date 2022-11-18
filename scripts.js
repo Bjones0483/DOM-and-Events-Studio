52388d3051c8c9027ffb7c74c60119b64709fa65
Bruce Jones <brucedjonesjr@gmail.com>
2022-11-17 20:25:23 -0600

    buttonDown

--- scripts.js
+++ scripts.js
@@ -7,6 +7,7 @@ window.addEventListener("load", function(){
     const statusFlight = document.getElementById("flightStatus");
     const heightShuttle = document.getElementById("spaceShuttleHeight");
     const backgroundShuttle = document.getElementById("shuttleBackground");
+    const buttonUp = this.document.getElementById('up')
 
     buttonTakeoff.addEventListener('click', function(){
         let confirmLaunch = window.confirm('Confirm that the shuttle is ready for takeoff.');
@@ -26,15 +27,29 @@ window.addEventListener("load", function(){
     })
 
     buttonAbort.addEventListener('click', function(){
-        let confirm
+        let confirm = window.confirm('Confirm you want to abort the mission.')
+        if (confirmLaunch){
         statusFlight.innerHTML = 'The shuttle has landed.';
         backgroundShuttle.style.backgroundColor = 'green';
         heightShuttle.innerHTML = 0;
+    }
     })
 
 
+buttonUp.addEventListener('click', function(){
+    if (statusFlight.innerHTML === 'shuttle in flight.'){
+        heightShuttle.innerHTML = add10k(Number(heightShuttle.innerHTML));
+    }
+    shuttleImg.style.marginBottom = 10;
+})
+console.log(parseInt(shuttleImg.style.marginBottom));
 
-
+buttonDown.addEventListener('click', function(){
+    if (statusFlight.innerHTML === "Shuttle in flight." && Number(heightShuttle.innerHTML) > 0){
+        heightShuttle.innerHTML = remove10k(Number(heightShuttle.innerHTML));
+    }
+});
+buttonLeft.addEventListener('click', function())
 function add10k(num){
     return num = 10000
 }
