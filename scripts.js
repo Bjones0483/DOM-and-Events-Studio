59a94796a3933107d759625296192823c7fd4e44
Bruce Jones <brucedjonesjr@gmail.com>
2022-11-17 19:44:05 -0600

    Script Info

--- scripts.js
+++ scripts.js
@@ -1,2 +1,41 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function(){
+    const buttonTakeoff = document.getElementById("takeoff");
+    const buttonLanding = document.getElementById("landing");
+    const buttonAbort = document.getElementById("missionAbort");
+    const statusFlight = document.getElementById("flightStatus");
+    const heightShuttle = document.getElementById("spaceShuttleHeight");
+    const backgroundShuttle = document.getElementById("shuttleBackground");
+
+    buttonTakeoff.addEventListener('click', function(){
+        let confirmLaunch = window.confirm('Confirm that the shuttle is ready for takeoff.');
+        if (confirmLaunch){
+            statusFlight.innerHTML = 'Shuttle in flight.';
+            backgroundShuttle.style.backgroundColor = 'blue';
+            heightShuttle.innerHTML = add10k(Number(heightShuttle.innerHTML));
+
+        }
+    })
+
+    buttonLanding.addEventListener('click', function(){
+        window.alert('The shuttle is landing. Landing gear engaged.');
+        statusFlight.innerHTML = 'The shuttle has landed.';
+        backgroundShuttle.style.backgroundColor = 'green';
+        heightShuttle.innerHTML = 0;
+    })
+
+    buttonAbort.addEventListener('click', function(){
+        let confirm
+        statusFlight.innerHTML = 'The shuttle has landed.';
+        backgroundShuttle.style.backgroundColor = 'green';
+        heightShuttle.innerHTML = 0;
+    })
+
+
+
+
+function add10k(num){
+    return num = 10000
+}
+})
